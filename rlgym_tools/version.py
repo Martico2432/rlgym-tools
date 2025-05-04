7394c7584837db445817e6f5bdb35aaa1ae5efaa
Martico2432 <mail.joks02@gmail.com>
2025-05-04 16:16:44 +0200

    Update version.py

--- rlgym_tools/version.py
+++ rlgym_tools/version.py
@@ -5,7 +5,7 @@
 # 3) we can import it into your module module
 # https://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package
 
-__version__ = '2.3.2'
+__version__ = '2.3.3'
 
 
 release_notes = {
